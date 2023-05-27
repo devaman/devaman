05629b35e072774ee06a28de51d25ca62021a1f0
Amit Chambial <chambialamit@gmail.com>
2023-05-27 13:52:04 +0530

    Update update_readme.js

--- scripts/js/update_readme.js
+++ scripts/js/update_readme.js
@@ -37,11 +37,13 @@ const fetchPosts = async() => {
 ![Hello everyone 👋](https://img.devaman.dev/2/?title=Hello%20Everyone%20%F0%9F%91%8B&website=github.com/devaman&back=f1d15b&textFill=fefefe&height=200)
 I am Amit Chambial , A Full stack developer. 
 
-I love to contribute to open-source software on GitHub. I am working for JP Morgan Chase as a Software Engineer I. 
+I love to contribute to open-source software on GitHub. I am working for Netomi as a Software Engineer II. 
 
 I have also worked as a Freelancer and I am a five star freelancer. 
 
 My moto is Make to Learn thats why i have made lots of projects as you can see. I love to make side projects. See my [Producthunt](https://www.producthunt.com/@amitchambial) and [Gumroad](https://gumroad.com/amit_chambial) seller page
+
+All my social link: [https://social.devaman.dev](https://social.devaman.dev)
 
 # My Blogs
 
